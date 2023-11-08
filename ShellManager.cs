e81d27ca6f6edc4397bfd132edd2dcb64de6aeec
sumas7 <smlcurreli@gmail.com>
2023-11-08 20:13:33 +0100

    first prototype on in-editor shell instancing
    - can generate and remove shells from editor
    - can generate more shells in the same place (needs fix)

--- ShellManager.cs
+++ ShellManager.cs
@@ -1,9 +1,12 @@
 using Godot;
 
+[Tool]
 public partial class ShellManager : Node3D
 {
 	[Export]
 	public int shellCount;
+	[Export]
+	public bool generateShells = false, destroyShells = false;
 
 	private int shellIndex = 0;
 	private float shellHeight = 0;
@@ -13,12 +16,44 @@ public partial class ShellManager : Node3D
 	{
 		shell_Scene = (PackedScene)GD.Load("res://Shell.tscn");
 
+		if(!Engine.IsEditorHint())
+			GenerateShells();
+	}
+    public override void _Process(double delta)
+    {
+		if(Engine.IsEditorHint())
+		{
+			if(generateShells)
+			{
+				GenerateShells();
+				generateShells = false;
+			}
+
+			if(destroyShells)
+			{
+				DestroyShells();
+				destroyShells = false;
+			}
+		}
+    }
+
+    void GenerateShells()
+	{
 		for(int i = 0; i < shellCount; i++)
 		{
 			MeshInstance3D shell = (MeshInstance3D)shell_Scene.Instantiate();
 			AddChild(shell);
 			shell.Position = new(0, shellHeight, 0);
 			shellHeight += 0.02f;
+		}
+		shellHeight = 0;
+	}
+
+	private void DestroyShells()
+	{
+		for(int i = 0; i < GetChildCount(); i++)
+		{
+			GetChild(i).QueueFree();
 		}
 	}
 }
