255e488efc3c0d86d9e2ee94701994f204ef82f9
sumas7 <smlcurreli@gmail.com>
2023-11-08 21:35:24 +0100

    Better in-editor instancing
    - you can't instance more than 1 time
    - you can only call DestroyShells() when there are shells
    - disabled occluision culling for all shells

--- ShellManager.cs
+++ ShellManager.cs
@@ -10,30 +10,44 @@ public partial class ShellManager : Node3D
 
 	private int shellIndex = 0;
 	private float shellHeight = 0;
+	private bool alreadyGenerated = false;
 	private PackedScene shell_Scene;
 
 	public override void _Ready()
 	{
 		shell_Scene = (PackedScene)GD.Load("res://Shell.tscn");
 
-		if(!Engine.IsEditorHint())
+		if(!Engine.IsEditorHint()) // if in game
+		{
+			DestroyShells();
 			GenerateShells();
+		}
 	}
     public override void _Process(double delta)
     {
-		if(Engine.IsEditorHint())
+		if(Engine.IsEditorHint()) // if in editor uses bools as buttons to instanciate and destroy shells
 		{
-			if(generateShells)
+			// if you press the button to generate and there are no shells, generates them
+			if(generateShells && !alreadyGenerated)
 			{
+				GD.Print("Shells generated");
 				GenerateShells();
+				alreadyGenerated = true;
 				generateShells = false;
 			}
+			else
+				generateShells = false;
 
-			if(destroyShells)
+			// if you press to destroy and there are shells, destroy them
+			if(destroyShells && alreadyGenerated)
 			{
+				GD.Print("Shells removed");
 				DestroyShells();
+				alreadyGenerated = false;
 				destroyShells = false;
 			}
+			else
+				destroyShells = false;
 		}
     }
 
@@ -44,14 +58,15 @@ public partial class ShellManager : Node3D
 			MeshInstance3D shell = (MeshInstance3D)shell_Scene.Instantiate();
 			AddChild(shell);
 			shell.Position = new(0, shellHeight, 0);
-			shellHeight += 0.02f;
+			shellHeight += 0.065f;
+			shell.Owner = GetTree().EditedSceneRoot;
 		}
 		shellHeight = 0;
 	}
 
 	private void DestroyShells()
 	{
-		for(int i = 0; i < GetChildCount(); i++)
+		for(int i = 0; i < GetChildCount(); i++) // destroys all children nodes
 		{
 			GetChild(i).QueueFree();
 		}
